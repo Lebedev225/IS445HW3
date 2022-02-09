98c0b878a270726dd753ef4f3a179be74ac4658d
Andrej <andrej.bosanac@student.csulb.edu>
2022-02-09 14:04:12 -0800

    Exercise 1 done

--- JS/ex1.js
+++ JS/ex1.js
@@ -2,4 +2,32 @@
     Example 1 JavaScript code
 */
 
-console.log("Bosanac's Output from Tutorial 4 Example 1");
+const userName = prompt("Enter your name: ")
+const unitsCompleted = prompt("Enter the number of units completed: ")
+
+if (unitsCompleted >= 0) {
+    if (unitsCompleted > 91) {
+        alert(`Hello ${userName}\nYour grade standing is Senior`)
+
+    }
+
+    else if (unitsCompleted >=61) {
+        alert(`Hello ${userName}\nYour grade standing is Junior`)
+
+    }
+
+    else if (unitsCompleted >=31) {
+        alert(`Hello ${userName}\nYour grade standing is Sophomore`)
+
+    }
+
+    else {
+        alert(`Hello ${userName}\nYour grade standing is Freshman`)
+
+    }
+}
+
+else {
+    alert("Invalid input, please use numbers >= 0")
+
+}
