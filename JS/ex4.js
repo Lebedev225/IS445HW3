d8e0cd6ec429e30723164add41feb33a2a8a5d99
Andrej <andrej.bosanac@student.csulb.edu>
2022-02-09 22:15:19 -0800

    ex4 and 5 work now

--- JS/ex4.js
+++ JS/ex4.js
@@ -1,6 +1,19 @@
 /* HW3
     Example 1 JavaScript code
 */
+const truePassword = "secret"
+let password = prompt("Enter your password: ")
+let i = 1
+while (password != truePassword && i != 3) {
+    password = prompt("Try again: ")
+    i++
+}
 
-const userName = prompt("Enter your name: ")
-const unitsCompleted = prompt("Enter the number of units completed: ")
+if (password == truePassword) {
+    alert("You entered the correct password after " + i + " attempt(s)")
+
+}
+
+else  {
+    alert("Your account is locked!  You failed to enter the correct password " + i + " times")
+}
---
+++ JS/ex5.js
@@ -0,0 +1,13 @@
+/* HW3
+    Example 1 JavaScript code
+*/
+
+let userNumber = Number(prompt("Enter your number: "))
+
+let multiplier = 0
+
+while (multiplier < 11)  {
+    console.log(`${userNumber} x ${multiplier} = ` + userNumber*multiplier)
+    multiplier++
+}
+
