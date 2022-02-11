ff076fd4022c5e1ebf35d143ad55e0520559377c
Andrej <andrej.bosanac@student.csulb.edu>
2022-02-10 17:58:51 -0800

    deleted test cases

--- JS/ex1.js
+++ JS/ex1.js
@@ -1,13 +1,8 @@
 /* HW3
     Example 1 JavaScript code
 */
-let i = 1;
 
-if ((1 > 2) && i++) {
-   //Nothing.  Want to test condition
-}
 
-console.log(`The value of i is: ${i}`);
 
 const userName = prompt("Enter your name: ")
 const unitsCompleted = prompt("Enter the number of units completed: ")
